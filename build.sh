9994f1b83c9944ce29c1e73565a3fac1176e2af7
torrin1 <torrin@worx4you.com>
2024-11-28 23:48:34 -0500

    reverting some changes

--- build.sh
+++ build.sh
@@ -1,59 +1,33 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command exits with a non-zero status
+set -e
 
-# Define user and directory variables
-USER="metal_deploy_prowlarr"
-HOME_DIR="/home/$USER"
-REPO_DIR="$HOME_DIR/repo"
-BUILD_DIR="$REPO_DIR/build"
-ARCHIVE_NAME="Prowlarr.tar.gz"
+apt update
+apt install -y curl sqlite3
 
-# Clean up previous builds and leftover files
-rm -rf "$BUILD_DIR"
-rm -f "$REPO_DIR/$ARCHIVE_NAME"
-
-# Ensure necessary packages are installed
-apt update && apt install -y curl sqlite3
-
-# Determine architecture
 arch=$(dpkg --print-architecture)
 case $arch in
 	amd64) arch="x64" ;;
 	arm|armf|armh) arch="arm" ;;
+	arm64) arch="arm64" ;;
 	*) echo "Unsupported architecture: $arch" && exit 1 ;;
 esac
 
-# Download Prowlarr package
-wget --content-disposition "http://prowlarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=$arch" -O "$REPO_DIR/$ARCHIVE_NAME"
+wget --content-disposition "http://prowlarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=$arch" -O ./Prowlarr.tar.gz
+tar -xvzf ./Prowlarr.tar.gz -C ./
 
-# Extract into the build directory
-mkdir -p "$BUILD_DIR"
-tar -xvzf "$REPO_DIR/$ARCHIVE_NAME" -C "$BUILD_DIR"
+mkdir -p ./build/
+mv ./Prowlarr ./build/Prowlarr/
+mkdir -p ~/.prowlarr-data
 
-# Create data directory
-mkdir -p "$BUILD_DIR/prowlarr-data"
-
-# Set ownership and permissions for the entire build directory
-chown -R $USER:$USER "$BUILD_DIR"
-chmod -R u+rwx "$BUILD_DIR"
-
-# Create run.sh script in the build directory
-cat <<'EOF' > "$BUILD_DIR/run.sh"
+cat <<'EOF' > ./build/run.sh
 #!/bin/bash
 
-# Define the base directory where your application is running
-BASE_DIR="$(dirname "$0")"
-
-# Convert the relative path to an absolute path
-DATA_DIR="$BASE_DIR/prowlarr-data"
-DATA_DIR=$(readlink -f "$DATA_DIR")
-
-# Run Prowlarr with an absolute path for the data directory
-exec "$BASE_DIR/Prowlarr" -nobrowser -data="$DATA_DIR"
+DATA_DIR=$(readlink -f "$HOME/.prowlarr-data")
+exec "./Prowlarr/Prowlarr" -nobrowser -data="$DATA_DIR"
 EOF
 
-chmod +x "$BUILD_DIR/run.sh"
-rm "$REPO_DIR/$ARCHIVE_NAME"
+chmod +x ./build/run.sh
+rm ./Prowlarr.tar.gz
 
-echo "Build complete. Run './repo/build/run.sh' to start Prowlarr."
+echo "Build complete. Run './build/run.sh' to start Prowlarr."
